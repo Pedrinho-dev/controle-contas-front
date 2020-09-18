d50cce28598333e6f851698b168cf7fb1659fbcc
Vinicius Souza <viniciuspodi@gmail.com>
2020-09-18 04:07:18 -0400

    Curso online first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container fluid>
+      <v-row justify="center">
+        <h1>Seja bem-vindo(a) ao controle de contas HT</h1>
+      </v-row>
+
+      <v-row>
+        <v-img
+          src="https://yt3.ggpht.com/a/AGF-l7_iPgV3tB-PxnLzTWvWnME5nhHD-vBkehcTWg=s900-mo-c-c0xffffffff-rj-k-no"
+          contain
+          aspect-ratio="1.7"
+          max-height="500px"
+        >
+        </v-img>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
